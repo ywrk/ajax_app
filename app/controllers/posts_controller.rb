46fa88779f2ab0c280071e653b2c61e0bb6b7296
mamoo3 <mamoo.wprg@gmail.com>
2020-10-31 16:12:55 +0900

    reset

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,15 +10,3 @@ class PostsController < ApplicationController
   end
 end
 
-def checked
-  post = Post.find(params[:id])
-  if post.checked 
-    post.update(checked: false)
-  else
-    post.update(checked: true)
-  end
-
-  item = Post.find(params[:id])
-  render json: { post: item }
-end
-
